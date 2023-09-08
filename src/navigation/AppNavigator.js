c2644af87c1e6994a34ac381c2c926c9bee6fd31
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-08 23:04:44 +0530

    recipee-ui

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import WelcomeScreen from '../screens/WelcomeScreen';
+
+const Stack = createNativeStackNavigator();
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const WelcomeScreen = () => {
+  return <View></View>;
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({});
